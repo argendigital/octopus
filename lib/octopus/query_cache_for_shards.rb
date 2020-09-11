d68f4cf71e93727911640201ed0367e592f7069a
Chris White <chris.white@codingzeal.com>
2020-09-11 14:08:14 -0600

    nil_check connection.shards when replacing enable/disable_query_cache!
    in our Rails 5.1 app in the test environment, ActiveJob#deliver_later throws an exception during callbacks when the base connection lacks shard info for an unknown reason

--- lib/octopus/query_cache_for_shards.rb
+++ lib/octopus/query_cache_for_shards.rb
@@ -1,10 +1,16 @@
 # query cache methods are moved to ConnectionPool for Rails >= 5.0
+#
+# when ActiveJob # deliver_later is invoked in test mode, this fails with
+# undefined method `shards' for #<ActiveRecord::ConnectionAdapters::PostgreSQLAdapter:0x00007facb9b9a2b8>
+# this is the reason for the respond_to? check
 module Octopus
   module ConnectionPool
     module QueryCacheForShards
       %i(enable_query_cache! disable_query_cache!).each do |method|
         define_method(method) do
-          if(Octopus.enabled? && (shards = ActiveRecord::Base.connection.shards)['master'] == self)
+          if(Octopus.enabled? &&
+            ActiveRecord::Base.connection.respond_to?(:shards) &&
+            (shards = ActiveRecord::Base.connection.shards)['master'] == self)
             shards.each do |shard_name, v|
               if shard_name == 'master'
                 super()
